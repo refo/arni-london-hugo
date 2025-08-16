12dc6b7488b01347603498e903f2dbf26be2397f
Refik ÖNAL <refikonal@gmail.com>
2025-08-16 18:05:38 +0300

    Updatr "products" buttons

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -30,7 +30,7 @@ export default class HomePreview extends React.Component {
           </div>
 
           <div className="tc">
-            <a href="#" className="btn raise">See all products</a>
+            <a href="#" className="btn raise">Read more</a>
           </div>
         </div>
       </div>
